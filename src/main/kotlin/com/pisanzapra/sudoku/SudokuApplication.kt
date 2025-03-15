3b52b7278b568991ecbf72859b1bdafd8c0729e5
lambda <shumpeta.kus@gmail.com>
2025-03-15 15:34:36 +0900

    initial commit

---
+++ src/main/kotlin/com/pisanzapra/sudoku/SudokuApplication.kt
@@ -0,0 +1,10 @@
+package com.pisanzapra.sudoku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication class SudokuApplication
+
+fun main(args: Array<String>) {
+  runApplication<SudokuApplication>(*args)
+}
